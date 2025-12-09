5eece4427e7d9e50333068f12c4e8dad52cee24d
William Ford <garrettford02@gmail.com>
2025-12-08 23:56:25 -0700

    Initial Commit

---
+++ Ford Outdoors/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Ford Outdoors
+//
+//  Created by William Garrett Ford on 12/8/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Ford Outdoors/Ford_OutdoorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ford_OutdoorsApp.swift
+//  Ford Outdoors
+//
+//  Created by William Garrett Ford on 12/8/25.
+//
+
+import SwiftUI
+
+@main
+struct Ford_OutdoorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
